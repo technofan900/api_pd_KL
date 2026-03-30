2944ddd382050d8d601feedf48c1246d1cb4b7fb
technofan900 <technofan900@gmail.com>
2026-03-30 10:18:59 +0300

    model ready

--- 2.daļa/src/models/RetPerson.php
+++ 2.daļa/src/models/RetPerson.php
@@ -2,5 +2,17 @@
 
 class RetPerson
 {
-    
+    private PDO $conn;
+
+    public function __construct(Database $db)
+    {
+        $this->conn = $db->getConnection();
+    }
+
+    public function get()
+    {
+        $sql = 'SELECT id, name, e_mail FROM person';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
